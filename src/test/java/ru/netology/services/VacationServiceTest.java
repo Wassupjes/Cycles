e6cf66d2f75dd27946b0a64df9ff57c01ef519c7
Alexey <wassupjes@mail.ru>
2024-02-26 22:50:28 +0300

    Added src

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,30 +1,18 @@
 package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void VacationThreeMonthsCounter() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void VacationMonthsCounter(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-        int actual = service.calculateVacations(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void VacationTwoMonthsCounter() {
-        VacationService service = new VacationService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
         int actual = service.calculateVacations(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
